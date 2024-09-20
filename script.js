a59fccf79b48959bbe9cb6503c1062f78807bdc7
jrm355 <jeremykazdan34@gmail.com>
2024-09-20 15:31:33 -0400

    part 3

--- script.js
+++ script.js
@@ -52,3 +52,21 @@ newLink.textContent = link.text
 // Append the new element to the topMenuEl element.
 topMenuEl.appendChild(newLink)
 })
+
+//Part 3
+// Select and cache the <nav id="sub-menu"> element in a variable named subMenuEl.
+let subMenuEl = document.getElementById('sub-menu');
+
+// Set the height subMenuEl element to be "100%".
+subMenuEl.style.height = "100%";
+
+// Set the background color of subMenuEl to the value stored in the --sub-menu-bg CSS custom property.
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
+
+// Add the class of flex-around to the subMenuEl element.
+subMenuEl.classList.add('flex-around');
+
+// Set the CSS position property of subMenuEl to the value of absolute.
+subMenuEl.style.position = "absolute";
+// Set the CSS top property of subMenuEl to the value of 0.
+subMenuEl.style.top = "0";
